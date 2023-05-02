6b89fd9e53387bb99dfd8341a2fdd41fcff6b4a4
KCrandall6 <kcrandall67@gmail.com>
2023-05-01 21:13:01 -0700

    proxy setup for running Solr; initial connection to Solr established

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,29 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap'
 
 const Search = () => {
 
+  const [results, setResults] = useState([])
+
   const queryResearch = (event) => {
     event.preventDefault();
-    fetch('grainsizeResearch.json')
-    .then((res) => res.json())
-    .then((res) => console.log('res', res))
-    console.log('test')
-  }
-  
+    const query = event.target.elements.query.value;
+    fetch(`/solr/research_papers/select?q=${query}&wt=json`)
+  .then((res) => res.json())
+  .then((res) => setResults(res.response.docs))
+  // .then((res) => console.log('res', res))
 
+  }
+
+  console.log('results', results)
+  
   return (
     <div className='d-flex mx-auto flex-column justify-content-center text-center p-5' style={{ maxWidth: '600px' }}>
-      <h1>Search area</h1>
-      <Form onSubmit={queryResearch}>
-      <Form.Group className="mb-3 mx-auto text-center" controlId="formBasicEmail">
-        <p className="m-4">Search within a collection of curated papers about the topic of sand grain analysis. The curated list is by no means the only list, but is at least a good starting point</p>
-        <Form.Control className="mx-auto" type="email" placeholder="Search Papers" style={{ maxWidth: '300px' }}/>
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-      </Form>
+      <div>
+        <h1>Search area</h1>
+        <Form onSubmit={queryResearch}>
+        <Form.Group className="mb-3 mx-auto text-center" controlId="formBasicEmail">
+          <p className="m-4">Search within a collection of curated papers about the topic of sand grain analysis. The curated list is by no means the only list, but is at least a good starting point</p>
+          <Form.Control className="mx-auto" name="query" placeholder="Search Papers" style={{ maxWidth: '300px' }}/>
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Search me
+        </Button>
+        </Form>
+      </div>
+      <div>
+        {results.length > 1 ? <h1>results</h1> : null}
+      </div>
     </div>
   )
 };
