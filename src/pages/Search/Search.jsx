3aa1bf86e0f958ca371f4cb53d13bb589c824ea5
KCrandall6 <kcrandall67@gmail.com>
2023-05-06 17:02:07 -0700

    updated search with instruction

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -5,17 +5,14 @@ const Search = () => {
 
   const [results, setResults] = useState([])
 
+  // To complete a search query, create a Solr instance and adjust query and query endpoint to your needs
   const queryResearch = (event) => {
     event.preventDefault();
     const query = event.target.elements.query.value;
-    fetch(`/solr/research_papers/select?q=${query}&wt=json`)
+    fetch(`${process.env.SOLR_URL}select?q=${query}&wt=json`)
   .then((res) => res.json())
   .then((res) => setResults(res.response.docs))
-  // .then((res) => console.log('res', res))
-
   }
-
-  console.log('results', results)
   
   return (
     <div className='d-flex mx-auto flex-column justify-content-center text-center p-5' style={{ maxWidth: '600px' }}>
@@ -24,6 +21,8 @@ const Search = () => {
         <Form onSubmit={queryResearch}>
         <Form.Group className="mb-3 mx-auto text-center" controlId="formBasicEmail">
           <p className="m-4">Search within a collection of curated papers about the topic of sand grain analysis. The curated list is by no means the only list, but is at least a good starting point</p>
+          <br/>
+          <p className="m-4"><em>To enable search, download the repo and create a local Solr instance following the instructions. Update the queryResearch variable with the correct query and endpoint as needed</em></p>
           <Form.Control className="mx-auto" name="query" placeholder="Search Papers" style={{ maxWidth: '300px' }}/>
         </Form.Group>
         <Button variant="primary" type="submit">
